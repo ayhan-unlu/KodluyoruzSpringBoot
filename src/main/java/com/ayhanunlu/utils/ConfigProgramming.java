1cd28e0e3b8f94a58d6fe17128dbc1aa94d42607
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-08-15 14:12:06 +0300

    DevTools activation

---
+++ src/main/java/com/ayhanunlu/utils/ConfigProgramming.java
@@ -0,0 +1,9 @@
+package com.ayhanunlu.utils;
+
+public class ConfigProgramming {
+//    DevTool ---> LiveReload
+//    file ---> settings --> build ---> compiler --> Build Project Automatically
+//    file ---> settings ---> advanced setting ---> compiler / Allow auto-make to start even
+
+
+}
