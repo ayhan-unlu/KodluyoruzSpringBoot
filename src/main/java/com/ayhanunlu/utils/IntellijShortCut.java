55e1e30860f663c1136bc9f13c913632fc5f369c
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-08-15 13:41:59 +0300

    IntellijShortCut

---
+++ src/main/java/com/ayhanunlu/utils/IntellijShortCut.java
@@ -0,0 +1,21 @@
+package com.ayhanunlu.utils;
+
+public class IntellijShortCut {
+    /*
+
+    settings -> command ö
+    generate constructor getter setter toString---> command n
+    comment line ---> option 7
+    comment lines ---> option 8
+
+
+
+     */
+
+/*
+    public int a;
+    public int b;
+    public int c;
+*/
+
+}
