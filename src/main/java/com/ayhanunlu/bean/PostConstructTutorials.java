ce26da9bc6d3a9f8749b32cc00518fcc659fd4f6
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-08-16 18:05:29 +0300

    PostConstructTutorials (Revised)

--- src/main/java/com/ayhanunlu/bean/PostConstructTutorials.java
+++ src/main/java/com/ayhanunlu/bean/PostConstructTutorials.java
@@ -9,8 +9,10 @@ import java.util.logging.Logger;
 @Controller
 public class PostConstructTutorials {
 
+/*
     @Autowired
     Logger LOG;
+*/
 
 
 //    No Args Constructor
@@ -23,15 +25,20 @@ public class PostConstructTutorials {
     // Bean not yet started, gives null pointer exception we receive null
     // dependency cant be injected(meaning autowired)
     // Duty of the PostConstruct annotation : When a bean object is created it s created just after.
+
+/*
     @PostConstruct
     public void init(){
         LOG.info("Log info is called");
     }
+*/
 
+/*
     public static void main(String[] args) {
 //        System.out.println("1");
         PostConstructTutorials beanController = new PostConstructTutorials();
 //        System.out.println("2");
         System.out.println(beanController);
     }
+*/
 }
