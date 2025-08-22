428c5aa3dabdd6be3c0941f11cbf5dbb7619c763
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-08-22 15:24:55 +0300

    Security included Security excluded

--- src/main/java/com/ayhanunlu/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/ayhanunlu/KodluyoruzSpringBootApplication.java
@@ -3,12 +3,15 @@ package com.ayhanunlu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Security active
+//@SpringBootApplication
+
 //Security inactive
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
 )
-//@SpringBootApplication
+
 public class KodluyoruzSpringBootApplication {
 
     public static void main(String[] args) {
